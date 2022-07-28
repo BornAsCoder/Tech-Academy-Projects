4ad26eb95cd53bb3f414585ecdf753ee21158196
BornAsCoder <104055035+BornAsCoder@users.noreply.github.com>
2022-07-28 07:44:57 +0100

    Assignment Submission # 2

---
+++ JavaScript/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFn() {
+    let a = 1;     //def and assign variable a
+    a += 3;        //concatenate variable a
+    document.getElementById("test").innerHTML = a;  //print variable a to element #test
+}
+
