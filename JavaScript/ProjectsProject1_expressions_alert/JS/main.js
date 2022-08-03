91d099b9469ea2ee7c864d3d611cf6ef6bb83459
BornAsCoder <104055035+BornAsCoder@users.noreply.github.com>
2022-08-03 03:32:37 +0100

    Delete JavaScript/ProjectsProject1_expressions_alert directory

--- JavaScript/ProjectsProject1_expressions_alert/JS/main.js
+++
@@ -1,7 +0,0 @@
-let str1 = "This is string1.", str2 = " This is red string2"; //Defining two variables and assigning a string values
-str2 = str2.fontcolor("red");   //Changing font colour of str2
-document.write(str1 + str2);    //Concatenating two variables and printing them on the webpage
-
-let result = 5 + 1;     //Defining variable and assignment expression
-window.alert("5 + 1 is " + result); //Print string and result variable in alert box
-
